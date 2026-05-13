91800b337796c7c64112b88522265d0675cdf06e
Dhrubshaw760 <dhrubshaw425@gmail.com>
2026-05-13 18:55:00 +0530

    Change

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import pickle
 
 # Load the trained model
-model = tf.keras.models.load_model('model.h5')
+model = tf.keras.models.load_model('model.keras')
 
 with open('scaler.pkl', 'rb') as f:
     scaler = pickle.load(f)
